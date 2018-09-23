0185c5b37399e6adc985da7ff1545ef66f0d74a7
YonasCodePartner <yonasbiochem@gmail.com>
2018-09-23 19:54:39 -0400

    add existin file

---
+++ main.js
@@ -0,0 +1,60 @@
+
+console.log(“hello world”);
+
+let box = “bottle”;
+console.log(“box = ” + box);
+
+box = “phone”;
+console.log(box);
+
+box = “wire”;
+console.log(box);
+
+
+let city = “harar”;
+console.log(city);
+
+city = “washington dc”;
+console.log(city)
+
+let speed = 50;
+
+if (speed<30){
+    console.log(“no ticket”)
+}
+else if(speed<=40)
+{
+    console.log(“50 dollar fine”)
+}
+else {
+    console.log(“100 dollar fine”)
+}
+
+let pro = prompt(“welcome to my page”);
+console.log(pro)
+
+
+let con = confirm(“Thank you for vist”)
+console.log(con)
+
+let al = alert(“wow we made it”)
+console.log(al)
+
+let questionOne = prompt (“Do you want to enter?“);
+
+if (questionOne == “yes”) {
+   let con = confirm (“WELCOME”);
+}
+
+else if (questionOne = “no”) {
+   questionOne = alert (“YOU DO NOT HAVE ACCESS”)
+}
+
+let pro = prompt (“Do you want to enter? (yes or no)“)
+console.log(pro)
+
+let con;
+   con = (“stop confirming!!!“);
+
+let al = alert (“alert alert alert alert.“);
+   console.log (alert);
